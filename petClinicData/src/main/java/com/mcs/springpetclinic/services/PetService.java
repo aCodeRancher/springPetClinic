fd52a16ea04a67c4c078de1827393e88dc00218b
mcsymiv <s.mcsymiv@gmail.com>
2022-04-05 21:26:08 +0300

    add service interfaces. Closes #5.

---
+++ petClinicData/src/main/java/com/mcs/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.mcs.springpetclinic.services;
+
+import com.mcs.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ petClinicData/src/main/java/com/mcs/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.mcs.springpetclinic.services;
+
+import com.mcs.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
