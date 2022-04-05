14ffaf63a482312bd083d75048e0145a8b5dc916
mcsymiv <s.mcsymiv@gmail.com>
2022-04-05 21:35:21 +0300

    add base entity. Closes #10.

--- petClinicData/src/main/java/com/mcs/springpetclinic/model/BaseEntity.java
+++ petClinicData/src/main/java/com/mcs/springpetclinic/model/BaseEntity.java
@@ -3,9 +3,12 @@ package com.mcs.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity {
+public class BaseEntity implements Serializable {
 
-    private String name;
+    private Long id;
+
 }
