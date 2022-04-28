c2091d32d3492b2a11a27c7dd38ad0bec7bdbcc8
mcsymiv <s.mcsymiv@gmail.com>
2022-04-28 20:02:51 +0300

    fix find owner query

--- petClinicData/src/main/java/com/mcs/springpetclinic/repository/OwnerRepository.java
+++ petClinicData/src/main/java/com/mcs/springpetclinic/repository/OwnerRepository.java
@@ -2,6 +2,8 @@ package com.mcs.springpetclinic.repository;
 
 import com.mcs.springpetclinic.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,9 +11,10 @@ public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    List<Owner> findAllByLastNameLike(String lastName);
+    List<Owner> findAllByLastNameLike(@Param("lastName") String lastName);
 
     List<Owner> findAllByLastNameContaining(String lastName);
 
+    @Query("SELECT DISTINCT owner FROM Owner owner WHERE lower(owner.lastName) LIKE lower(concat('%', :lastName, '%'))")
     List<Owner> findAllByLastNameContainingIgnoreCase(String lastName);
 }
