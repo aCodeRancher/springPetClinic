82b1cf3a0ad3fa487d6aa17e441ac7485d238956
mcsymiv <s.mcsymiv@gmail.com>
2022-05-06 15:51:17 +0300

    impl vet json. close #58

--- petClinicWeb/src/main/java/com/mcs/springpetclinic/controllers/vet/VetController.java
+++ petClinicWeb/src/main/java/com/mcs/springpetclinic/controllers/vet/VetController.java
@@ -1,10 +1,15 @@
 package com.mcs.springpetclinic.controllers.vet;
 
+import com.mcs.springpetclinic.model.Vet;
 import com.mcs.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -20,5 +25,10 @@ public class VetController {
     public String getAllVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody List<Vet> vetJson() {
+        return vetService.findAll();
     }
 }
