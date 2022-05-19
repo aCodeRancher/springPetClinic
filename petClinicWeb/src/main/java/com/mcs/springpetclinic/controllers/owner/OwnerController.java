8acc196146818ad6f6d009c1cfb2a0c517b26aad
helen <hhma@alumni.cpp.edu>
2022-05-19 18:51:14 -0400

    Use findAllByLastNameLike instead

--- petClinicWeb/src/main/java/com/mcs/springpetclinic/controllers/owner/OwnerController.java
+++ petClinicWeb/src/main/java/com/mcs/springpetclinic/controllers/owner/OwnerController.java
@@ -47,8 +47,8 @@ public class OwnerController {
         }
 
         // find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameContainingIgnoreCase(owner.getLastName());
-
+        //List<Owner> results = ownerService.findAllByLastNameContainingIgnoreCase(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
         if (results.isEmpty()) {
             // no owners found
             result.rejectValue("lastName", "notFound", "not found");
